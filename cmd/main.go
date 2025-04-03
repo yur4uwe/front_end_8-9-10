cf1605c594592161c9c59202e2cc3802ca6a75a7
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-04-03 15:43:24 +0300

    gracefull server shutdown implemented

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("../frontend/build"))
 
-	http.Handle("/", fs)
+	handler := http.NewServeMux()
+	handler.Handle("/", fs)
 
-	fmt.Println("Server started at http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+
+	go func() {
+		fmt.Println("Server started at http://localhost:8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Error starting server:", err)
+		}
+	}()
+
+	<-exit
+	fmt.Println("Shutting down server...")
+
+	if err := server.Close(); err != nil {
+		fmt.Println("Error shutting down server:", err)
+	} else {
+		fmt.Println("Server gracefully stopped")
 	}
 }
